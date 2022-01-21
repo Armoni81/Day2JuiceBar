2b62a98b92647c00a043915e05901127e978d074
Armoni Tigner <atigner2@student.gsu.edu>
2022-01-20 21:20:17 -0500

    finished problem 2

--- day2.js
+++ day2.js
@@ -8,7 +8,7 @@ all of nothing takes 5 minutes
 every other drink takes 2.5 minutes
 */
 
-function timeToMixJuice(juice){
+/*function timeToMixJuice(juice){
   var waitTime = 0
     switch(juice){
         case "Pure Strawberry Joy": 
@@ -30,6 +30,42 @@ function timeToMixJuice(juice){
     
 }
 
-console.log(timeToMixJuice("Green Garden")) 
+console.log(timeToMixJuice("Jay")) */
+/*She can get 6 wedges from a 'small' lime, 8 wedges from a 
+'medium' lime and 
+10 from a 'large' lime.*/
+
+function limesToCut(wedgesNeeded, limes){
+    console.log("wedges i need today " + wedgesNeeded)
+let numberOfLimesCut  = 0
+let wedgesCount  = 0
+/*for (i = 0; i< limes.length; i++){
+    console.log("Using for loop;"+limes[i])
+} */
+
+var i = 0
+while(wedgesCount <= wedgesNeeded  && i< limes.length){
+    if ( limes[i]== "small"){
+        wedgesCount +=  6
+        numberOfLimesCut += 1
+        console.log("im cutting  a small lime")
+    }else  if( limes[i]== "medium"){
+        wedgesCount +=  8
+        numberOfLimesCut += 1
+        console.log("im cutting a medium lime")
+    }else if(limes[i]==  "large"){
+        wedgesCount +=  10
+        numberOfLimesCut += 1
+        console.log("im cutting a large lime")
+    }
+    //console.log("Using while loop;"+limes[i])
+    i++;
+
+}
+console.log("Wedges on my table  " +wedgesCount )
 
 
+    return numberOfLimesCut
+}
+
+console.log("Limes needed to cut  " + limesToCut(25,['small', 'small', 'large', 'medium', 'small']))
